80553c302080b3031261f01fc72bfde7db7dea52
xiongyejun <244114746@qq.com>
2018-05-31 22:13:58 +0800

    thumbs.db

--- src/pkgMyPkg/compoundFile/getItems.go
+++ src/pkgMyPkg/compoundFile/getItems.go
@@ -3,13 +3,19 @@ package compoundFile
 import (
 	"errors"
 	"fmt"
+	"pkgMyPkg/colorPrint"
+	"strconv"
 	"strings"
+	"unicode"
 )
 
 //func (me *CompoundFile) GetStorage(StorageName string) (s *Storage, err error) {
 //	return nil, nil
 //}
 // [6]DataSpaces\DataSpaceInfo\streamName
+
+var cp *colorPrint.ColorDll
+
 func (me *CompoundFile) GetStream(streamPath string) (b []byte, err error) {
 	var s *Storage = me.Root
 	var arr []string = strings.Split(streamPath, `\`)
@@ -23,7 +29,7 @@ func (me *CompoundFile) GetStream(streamPath string) (b []byte, err error) {
 	}
 
 	if index, ok := s.streamDic[arr[len(arr)-1]]; !ok {
-		return nil, errors.New("不存在的Stream名称。")
+		return nil, errors.New("不存在的Stream名称。" + arr[len(arr)-1])
 	} else {
 		// 读取stream是按照512的大小读取的，但最后1个可能没有512
 		return s.Streams[index].stream.Bytes()[:s.streamSize[index]], nil
@@ -31,11 +37,14 @@ func (me *CompoundFile) GetStream(streamPath string) (b []byte, err error) {
 }
 
 func (me *CompoundFile) PrintOut() {
+	cp = colorPrint.NewColorDll()
 	me.printOut(me.Root, "")
 }
 
 func (me *CompoundFile) printOut(s *Storage, strPre string) {
-	fmt.Printf("%s%s[Storage]\r\n", strPre, firstIs6(s.dir.name))
+	cp.SetColor(colorPrint.White, colorPrint.DarkYellow)
+	fmt.Printf("%s%s [Storage]\r\n", strPre, getPrintString(s.dir.name))
+	cp.UnSetColor()
 
 	for i := range s.Storages {
 		//		fmt.Printf("%s%s\r\n", strPre+"--", s.Storages[i].dir.name)
@@ -43,13 +52,40 @@ func (me *CompoundFile) printOut(s *Storage, strPre string) {
 	}
 
 	for i := range s.Streams {
-		fmt.Printf("%s%s[Stream]\r\n", strPre+"----", firstIs6(s.Streams[i].name))
+		fmt.Printf("%s%s [Stream]\r\n", strPre+"----", getPrintString(s.Streams[i].name))
 	}
 }
 
-func firstIs6(str string) string {
-	if str[0] == 6 {
-		return "[6]" + str[1:]
+func getPrintString(str string) string {
+	var strRet string
+
+	for _, r := range []rune(str) {
+		if !unicode.IsPrint(r) {
+			strRet = strRet + "[" + strconv.Itoa(int(r)) + "]"
+		} else {
+			strRet += string(r)
+		}
 	}
-	return str
+
+	return strRet
+}
+
+type retStream struct {
+	B    []byte
+	Name string
+}
+
+// 保存所有的stream
+func (me *CompoundFile) GetStreams() (ret []retStream) {
+	ret = make([]retStream, len(me.cfs.arrStream))
+	var k int = 0
+	for i := range me.cfs.arrStream {
+		if me.cfs.arrStream[i].stream != nil {
+			ret[k].Name = me.cfs.arrStream[i].name
+			ret[k].B = me.cfs.arrStream[i].stream.Bytes()
+			k++
+		}
+
+	}
+	return ret[:k]
 }
