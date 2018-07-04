e1eb81a037a059ec0dbec518dd2cd54497af7ec6
xiongyejun <244114746@qq.com>
2018-07-04 23:29:43 +0800

    oc

--- src/oc/officeCrypto.go
+++ src/oc/officeCrypto.go
@@ -5,12 +5,14 @@ package main
 import (
 	"bufio"
 	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"pkgMyPkg/compoundFile"
 	"pkgMyPkg/offCrypto"
+	"pkgMyPkg/permuCombin"
 	"runtime"
 	"strconv"
 	"time"
@@ -21,19 +23,37 @@ var arrCount []uint64                  // 统计已经测试的个数
 
 var ies []offCrypto.IEncryptedType
 var chanPsw chan []byte
-var flag bool = false
+var bflag bool = false
+var totalsum uint64 = 0
+
+var bKey *bool
+var bPermu *bool
 
 func main() {
-	fmt.Println("officeCrypto <FileName> <Password txt>")
-	if len(os.Args) != 3 {
+	fmt.Println("officeCrypto <-k或-p> <FileName> <key或srcPermu的txt> <selectCount>\r\nsrcPermu的数据源一行一个,-p时候还要设置selectCount。")
+	if *bKey && *bPermu {
+		fmt.Println("-k和-p只能设置一个。")
 		return
+	} else if !*bKey && !*bPermu {
+		fmt.Println("-k和-p至少设置一个。")
+		return
+	}
+
+	if *bKey {
+		if len(os.Args) != 4 {
+			return
+		}
+	} else {
+		if len(os.Args) != 5 {
+			return
+		}
 	}
 
 	// 读取检查密码所需数据
 	var bEncryptionInfo []byte
 	var err error
 	var bECMA376 bool = true
-	if bEncryptionInfo, bECMA376, err = readEncryptionInfo(os.Args[1]); err != nil {
+	if bEncryptionInfo, bECMA376, err = readEncryptionInfo(os.Args[2]); err != nil {
 		fmt.Println(err)
 		return
 	}
@@ -46,14 +66,36 @@ func main() {
 			return
 		}
 	}
-	// 读取记录密码的txt文件
+
+	// 获取测试的密码
 	chanPsw = make(chan []byte, COUNT_CHECK*10)
-	if f, err := os.Open(os.Args[2]); err != nil {
-		fmt.Println(err)
-		return
+	if *bKey {
+		// 读取记录密码的txt文件
+		if f, err := os.Open(os.Args[3]); err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			defer f.Close()
+			go readPassword(chanPsw, f)
+		}
 	} else {
-		defer f.Close()
-		go readPassword(chanPsw, f)
+		//  使用排列组合方式
+		var src []string
+		if f, err := os.Open(os.Args[3]); err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			defer f.Close()
+			src = readPermuSrc(f)
+		}
+		if selectCount, err := strconv.Atoi(os.Args[4]); err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			fmt.Println("src =", src)
+			go permuCombin.PermuString(src, uint(selectCount), chanPsw)
+			totalsum = uint64(getCount(len(src), int(selectCount)))
+		}
 	}
 
 	// 开始测试密码
@@ -62,25 +104,61 @@ func main() {
 		go checkPassword(ies[i], chanPsw, &arrCount[i])
 	}
 
-	timeStart := time.Now()
-	for !flag {
-		time.Sleep(0.01 * 1e9)
-		printSumCount(timeStart)
+	var iTime int64 = 0
+	for !bflag {
+		time.Sleep(1 * 1e9)
+		iTime++
+		var sum uint64 = getSumCount()
+		fmt.Printf("\r正在测试第%10s个密码，用时：%s……", formatUint64(sum), formatTime(iTime))
+		if sum == totalsum {
+			fmt.Println("\r\n未能找到密码！")
+			return
+		}
 	}
 
-	printSumCount(timeStart)
 	fmt.Println("\r\n未能找到密码！")
 }
 
+func init() {
+	bKey = flag.Bool("k", false, "读取密码字典")
+	bPermu = flag.Bool("p", false, "排列组合")
+
+	//	flag.PrintDefaults()
+	flag.Parse()
+}
+
+// ie		IEncryptedType密码测试的接口
+// chanPsw	记录密码的channel
+// count	统计测试的密码的个数
+func checkPassword(ie offCrypto.IEncryptedType, chanPsw chan []byte, count *uint64) {
+	var pswUnicode []byte
+
+	for {
+		pswUnicode = <-chanPsw
+		//		tmp := []byte("1")
+		//		pswUnicode = append(tmp, pswUnicode...)
+		pswUnicode = asc2Unicode(pswUnicode)
+		if err := ie.CheckPassword(pswUnicode); err == nil {
+			if b, err := unicode2Asc(pswUnicode); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("\r\n找到密码：%s\r\n", b)
+				bflag = true
+			}
+			os.Exit(0)
+		}
+		*count++
+	}
+}
+
 // 输出测试了多少个
-func printSumCount(timeStart time.Time) {
+func getSumCount() uint64 {
 	// 统计测试的总个数
 	var sum uint64 = 0
 	for i := 0; i < COUNT_CHECK; i++ {
 		sum += arrCount[i]
 	}
-
-	fmt.Printf("\r正在测试第%10s个密码，用时：%s……", formatUint64(sum), formatTime(time.Now().Unix()-timeStart.Unix()))
+	return sum
 }
 
 // 读取解密所需的数据
@@ -150,28 +228,6 @@ func formatTime(second int64) string {
 	return fmt.Sprintf("%2d时%2d分%2d秒", h, m, second)
 }
 
-// ie		IEncryptedType密码测试的接口
-// chanPsw	记录密码的channel
-// count	统计测试的密码的个数
-func checkPassword(ie offCrypto.IEncryptedType, chanPsw chan []byte, count *uint64) {
-	var pswUnicode []byte
-
-	for {
-		*count++
-		pswUnicode = <-chanPsw
-		if err := ie.CheckPassword(pswUnicode); err == nil {
-			if b, err := unicode2Asc(pswUnicode); err != nil {
-				fmt.Println(err)
-			} else {
-				fmt.Printf("\r\n找到密码：%s\r\n", b)
-				flag = true
-			}
-			os.Exit(0)
-		}
-
-	}
-}
-
 // 从f中读取密码，放入chanPsw
 func readPassword(chanPsw chan []byte, f *os.File) {
 	bf := bufio.NewReader(f)
@@ -180,11 +236,22 @@ func readPassword(chanPsw chan []byte, f *os.File) {
 		if err == io.EOF {
 			break
 		}
-		b = asc2Unicode(b)
 		chanPsw <- b
+		totalsum++
 	}
-	// 读完了密码字典txt
-	flag = true
+}
+
+func readPermuSrc(f *os.File) (src []string) {
+	src = make([]string, 0)
+	bf := bufio.NewReader(f)
+	for {
+		b, _, err := bf.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		src = append(src, string(b))
+	}
+	return
 }
 
 // asc的byte转换为unicode的byte
@@ -208,4 +275,12 @@ func unicode2Asc(b []byte) (bb []byte, err error) {
 		bb[i/2] = b[i]
 	}
 	return bb, nil
+}
+
+func getCount(m, n int) (icount int) {
+	icount = 1
+	for i := 0; i < n; i++ {
+		icount *= m
+	}
+	return
 }
